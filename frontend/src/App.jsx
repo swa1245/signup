cef3b5eeb3ac6359e33d07886989881b5a9c4d36
swa1245 <swarajpatil2123@gmail.com>
2025-04-11 22:27:40 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
